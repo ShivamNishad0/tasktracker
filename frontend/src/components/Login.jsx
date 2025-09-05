7adf032cfdbd7b7eae7e83ba1d6ae373a2f07c68
Shivam Nishad <154549545+ShivamNishad0@users.noreply.github.com>
2025-09-05 12:15:31 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -294,7 +294,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
     ];
 
     return (
-        <div className="max-h-screen flex items-center justify-center bg-gray-100">
+        <div className="min-h-screen w-screen flex items-center justify-center bg-gray-100">
             <div className='max-w-md w-full bg-white shadow-lg border border-purple-100 rounded-xl p-8'>
                <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
 
